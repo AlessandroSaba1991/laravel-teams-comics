435cc97b3d0b322d9e38037a9302babd7426d4d2
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-06-20 16:24:41 +0200

    fix name controller

--- app/Http/Controllers/Guest/GuestCharacterController.php
+++ app/Http/Controllers/Guest/GuestCharacterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class GuestCharacterController extends Controller
+class CharacterController extends Controller
 {
     //
 }
