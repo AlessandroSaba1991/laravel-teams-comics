3d9fde74b27f153935d6b90f59bc052a0cd7ed5a
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-20 16:41:23 +0200

    6- Develop Guest/CharactersController

--- app/Http/Controllers/Guest/CharacterController.php
+++ app/Http/Controllers/Guest/CharacterController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Character;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CharacterController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function list()
+    {
+        $characters = Character::all();
+        return view('character.index', compact('characters'));
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Character $character)
+    {
+        return view('character.show', compact('character'));
+    }
 }
