14da2637a3ea654fef3fe749b9983f1d6ac9b14d
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-06-27 17:57:03 +0200

    16- sistemare tabella , seed, model comic close #24

--- database/migrations/2022_06_20_141843_create_comics_table.php
+++ database/migrations/2022_06_20_141843_create_comics_table.php
@@ -16,10 +16,10 @@ class CreateComicsTable extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title',50);
+            $table->string('slug', 100);
             $table->text('description');
             $table->string('thumb');
             $table->decimal('price', 8, 2);
-            $table->string('series',50);
             $table->date('sale_date');
             $table->string('type',50);
             $table->timestamps();
