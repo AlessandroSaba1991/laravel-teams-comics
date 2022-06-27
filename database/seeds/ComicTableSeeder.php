174260b8892241538e8f839b19f4764428f13ce9
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-06-27 18:34:03 +0200

    fix namespace comic model

--- database/seeds/ComicTableSeeder.php
+++ database/seeds/ComicTableSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Comic;
+use App\Models\Comic;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CharacterTableSeeder::class);
-        $this->call(ComicTableSeeder::class);
+        $this->call([
+            ComicTableSeeder::class,
+            SerieSeeder::class
+        ]);
 
     }
 }
